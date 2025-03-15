892435970f161a01fbfacf6c9fbe7495a915011b
Aaron Palacios Olea <apalacios.dev@gmail.com>
2025-03-15 20:39:57 +0100

    basic .bat generator for "FOUR" layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { LayoutType } from './shared/models/layout.model';
 import { LayoutSelectorComponent } from "./components/layout-selector/layout-selector.component";
 import { CommonModule } from '@angular/common';
 import { TerminalComponent } from "./components/terminal/terminal.component";
+import { BatService } from './shared/services/bat.service';
 
 @Component({
   selector: 'app-root',
@@ -19,8 +20,10 @@ export class AppComponent {
   layoutType = LayoutType;
 
   data = {
-    layout: LayoutType.ONE_LEFT_TWO_RIGHT,
-    gridCommands: [[], [], []],
+    layout: LayoutType.FOUR,
+    gridCommands: [[], [], [], []],
     gridSelected: 1
   }
+
+  constructor(public batService: BatService) {}
 }
---
+++ src/app/shared/services/bat.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { LayoutType } from '../models/layout.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BatService {
+
+  generateBatFile(data: any): void {
+    const batContent = this.generateLayout(data);
+
+    const blob = new Blob([batContent], { type: 'text/plain' });
+
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = 'start_terminal.bat';
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+
+
+
+
+  generateLayout(data: any): string { 
+    const commands = data.gridCommands.map((grid: string[]) => 
+      grid.reduce((acc: string, command: string, index: number) => 
+        acc + (index > 0 ? ' && ' : '') + command, '')
+    );
+
+    switch (data.layout) {
+      case LayoutType.FOUR:
+        return `
+          @echo off
+          start wt new-tab cmd /k "${commands[0]}" ^
+          ; split-pane -V  cmd /k "${commands[1]}" ^
+          ; move-focus left                        ^
+          ; split-pane -H  cmd /k "${commands[2]}" ^
+          ; move-focus right                       ^
+          ; split-pane -H  cmd /k "${commands[3]}"
+        `;
+
+      default:
+        return `
+          @echo off
+          start wt new-tab cmd /k "cd C:\Downloads && dir" ^
+          ; split-pane -V  cmd /k "cd /d C:\Users\Dev\Downloads"         ^
+          ; move-focus left                                       ^
+          ; split-pane -H  cmd /k "cd /d C:\Users\Dev\Downloads"      ^
+          ; move-focus right                                      ^
+          ; split-pane -H  cmd /k "cd /d C:\Users\Dev\Downloads"
+        `;
+    }
+  }
+
+
+}
